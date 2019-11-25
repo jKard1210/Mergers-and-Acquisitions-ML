ff84fa991b922e8f6823b01b8d2986b782c3ad5c
James Kardatzke <jameskardatzke@Jamess-MacBook-Pro.local>
2019-11-25 11:27:15 -0600

    update

--- Modeling/daskML.py
+++ Modeling/daskML.py
@@ -6,147 +6,131 @@ import matplotlib.pyplot as plt
 import dask.dataframe as dd
 from dask.distributed import Client
 
-
-def run2():
-    import sqlite3
-
-    connection = sqlite3.connect("profitability.db")
-    crsr = connection.cursor()
-
-    attachDatabaseSQL        = "ATTACH DATABASE ? AS acquisitions"
-
-    dbSpec  = ("acquisitions.db",)
-
-    crsr.execute(attachDatabaseSQL,dbSpec)
-    crsr.execute('''SELECT * from pg1 left join acquisitions on pg1.ticker=acquisitions.ticker and pg1.date = acquisitions.date''')
-    rows = crsr.fetchall()
-    print(np.array(rows))
-    djlfskjdl
-    for row in rows:
-        print(row)
-    connection.commit()
-
-
-def run3():
+def runRF():
     client = Client()
     from dask_ml.datasets import make_classification
-    df = dd.read_csv("isHealthTrain.csv",assume_missing=True,sample=640000000,blocksize="10MB")
+
+    df = dd.read_csv("train5.csv",assume_missing=True,sample=640000000,blocksize="10MB")
     df = df.fillna(0).fillna(0)
     for column in df.columns:
         if '.' in column:
             df = df.drop(column,axis=1)
-    # for column in droppedColumns:
-    #     df = df.drop(column, axis=1)
-    y_train = df['acquired']
-    X_train = df.drop('acquired',axis=1)
-    df2 = dd.read_csv("isHealthTest.csv",assume_missing=True,sample=640000000,blocksize="10MB")
+    y_train = df['acquiredNew']
+    print(y_train)
+    X_train = df.drop('acquiredNew',axis=1)
+    X_train = X_train.drop('SIC',axis=1)
+    X_train = X_train.drop('CIK',axis=1)
+    df2 = dd.read_csv("test5.csv",assume_missing=True,sample=640000000,blocksize="10MB")
     df2 = df2.fillna(0).fillna(0)
     for column in df2.columns:
         if '.' in column:
             df2 = df2.drop(column,axis=1)
-    # for column in droppedColumns:
-    #     df = df.drop(column, axis=1)
-    y_test = df2['acquired']
-    X_test = df2.drop('acquired',axis=1)
-    # X_train,X_train2,y_train,y_train2 = train_test_split(X_train,y_train)
+
+    y_test = df2['acquiredNew']
+    X_test = df2.drop('acquiredNew',axis=1)
+    X_test = X_test.drop('SIC',axis=1)
+    X_test = X_test.drop('CIK',axis=1)
     x_test_tickers = X_test['ticker'].values.compute()
     x_test_dates = X_test['date'].values.compute()
     print(x_test_tickers[0])
+
     np.savetxt("x_test_tickers.csv",x_test_tickers,delimiter=",",fmt='%s')
     np.savetxt("x_test_dates.csv",x_test_dates,delimiter=",",fmt='%s')
     print("GOOD")
+
     for column in X_train.columns:
         if 'ticker' in column or 'date' in column:
             X_train = X_train.drop(column,axis=1)
             X_test = X_test.drop(column,axis=1)
+
     X_train = X_train.to_dask_array()
     X_test = X_test.values.compute()
     y_train = y_train.to_dask_array()
     y_test = y_test.values.compute()
+
     np.savetxt("y_test.csv", y_test, delimiter=",")
+
     from dask_ml.wrappers import Incremental
     from sklearn.linear_model import SGDClassifier
     from sklearn.neural_network import MLPClassifier
     from dask_ml.wrappers import ParallelPostFit
+    from sklearn.linear_model import LogisticRegression
+    from sklearn.ensemble import RandomForestClassifier
+    clf = RandomForestClassifier(n_estimators=100, max_depth=4,random_state=0)
+    clf.fit(X_train,y_train)
+    np.savetxt("featureimportance.csv",clf.feature_importances_)
+    predictions = clf.predict(X_test)
+    np.savetxt("predictions.csv",predictions)
+    np.savetxt("predictionsProba.csv",clf.predict_proba(X_test))
 
-    est = MLPClassifier(solver='adam',activation='relu', random_state=0)
-    print(est)
-    inc = Incremental(est, scoring='f1')
-    print("WORKING")
-    for _ in range(10):
-        inc.partial_fit(X_train, y_train, classes=[0,1])
-        print("FITTED")
-        np.savetxt("predictions.csv",inc.predict_proba(X_test))
-        print('Score:', inc.score(X_test, y_test))
-
-
-    # params = {'alpha': np.logspace(-2, 1, num=1000)}
-    # from dask_ml.model_selection import IncrementalSearchCV
-    # search = IncrementalSearchCV(est, params, n_initial_parameters=100,patience=20, max_iter=100)
-    # search.fit(X_train, y_train, classes=[0, 1])
-    # print(search)
-    # print("SCORE")
-    # print("FITTED")
-    # np.savetxt("predictions.csv",search.predict_proba(X_test))
-    # print('Score:', search.score(X_test, y_test))
-
-
-
-def run():
+def runMLP():
     client = Client()
     from dask_ml.datasets import make_classification
-    df = dd.read_csv("isHealth.csv",assume_missing=True,sample=640000000,blocksize="10MB")
+
+    df = dd.read_csv("train5.csv",assume_missing=True,sample=640000000,blocksize="10MB")
     df = df.fillna(0).fillna(0)
     for column in df.columns:
         if '.' in column:
             df = df.drop(column,axis=1)
-    # for column in droppedColumns:
-    #     df = df.drop(column, axis=1)
-    y = df['acquired']
-    X = df.drop('acquired',axis=1)
-    from dask_ml.model_selection import train_test_split
-    X_train, X_test, y_train, y_test = train_test_split(X, y,test_size=.1)
-    # X_train,X_train2,y_train,y_train2 = train_test_split(X_train,y_train)
+    y_train = df['acquiredNew']
+    print(y_train)
+    X_train = df.drop('acquiredNew',axis=1)
+    X_train = X_train.drop('SIC',axis=1)
+    X_train = X_train.drop('CIK',axis=1)
+    df2 = dd.read_csv("test5.csv",assume_missing=True,sample=640000000,blocksize="10MB")
+    df2 = df2.fillna(0).fillna(0)
+    for column in df2.columns:
+        if '.' in column:
+            df2 = df2.drop(column,axis=1)
+
+    y_test = df2['acquiredNew']
+    X_test = df2.drop('acquiredNew',axis=1)
+    X_test = X_test.drop('SIC',axis=1)
+    X_test = X_test.drop('CIK',axis=1)
     x_test_tickers = X_test['ticker'].values.compute()
     x_test_dates = X_test['date'].values.compute()
     print(x_test_tickers[0])
-    np.savetxt("x_test_tickers.csv",[x_test_tickers,x_test_dates],delimiter=",",fmt='%s')
+
+    np.savetxt("x_test_tickers.csv",x_test_tickers,delimiter=",",fmt='%s')
     np.savetxt("x_test_dates.csv",x_test_dates,delimiter=",",fmt='%s')
     print("GOOD")
+
     for column in X_train.columns:
         if 'ticker' in column or 'date' in column:
             X_train = X_train.drop(column,axis=1)
             X_test = X_test.drop(column,axis=1)
+
     X_train = X_train.to_dask_array()
     X_test = X_test.values.compute()
     y_train = y_train.to_dask_array()
     y_test = y_test.values.compute()
+
     np.savetxt("y_test.csv", y_test, delimiter=",")
+    
     from dask_ml.wrappers import Incremental
     from sklearn.linear_model import SGDClassifier
     from sklearn.neural_network import MLPClassifier
     from dask_ml.wrappers import ParallelPostFit
+    from sklearn.linear_model import LogisticRegression
+    from sklearn.ensemble import RandomForestClassifier
 
-    est = MLPClassifier(solver='adam',activation='relu', random_state=0)
-    inc = Incremental(est, scoring='neg_log_loss')
+    est  = MLPClassifier(solver='adam',activation='relu', alpha = 0.0001,random_state=10,max_iter=2000)
+    inc = Incremental(est)
     print("WORKING")
-    for _ in range(10):
-        inc.partial_fit(X_train, y_train, classes=[0,1])
-        print("FITTED")
-        np.savetxt("predictions.csv",inc.predict_proba(X_test))
+    for _ in range(100):
+        inc.fit(X_train, y_train, classes=[0,1])
+        for j in range(0, 20):
+            print("FITTED")
+        predictions = inc.predict(X_test)
+        print("Number Positive: ", np.sum(predictions))
+        print("TOTAL: ", len(predictions))
+        np.savetxt("predictions.csv",predictions)
+        np.savetxt("predictionsProba.csv",inc.predict_proba(X_test))
+        print('Score:', inc.score(X_train, y_train))
         print('Score:', inc.score(X_test, y_test))
 
 
-    # model = MLPClassifier(solver='sgd', hidden_layer_sizes=(10,2),random_state=1)
-    params = {'alpha': np.logspace(-2, 1, num=1000)}
-    from dask_ml.model_selection import IncrementalSearchCV
-    search = IncrementalSearchCV(est, params, n_initial_parameters=100,patience=20, max_iter=100)
-    search.fit(X_train, y_train, classes=[0, 1])
-    print(search)
-    print("SCORE")
-    print("FITTED")
-    np.savetxt("predictions.csv",inc.predict_proba(X_test))
-    print('Score:', inc.score(X_test, y_test))
 
 if __name__ == '__main__':
-    run3()
+    runRF()
+    runMLP()
